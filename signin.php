c12af3cae9704c40960ff28b3561fc643b7f8fd9
Immie <immaculateatim56@gmail.com>
2024-03-28 17:51:25 +0300

    Organised the sumbmit button.

--- signin.php
+++ signin.php
@@ -148,7 +148,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="centered-text">
                 <p>Don't have an account? <a href="signup.php">Sign Up now</a>.</p>
                 </div>
-
             </form>
         </div>
     </div>
